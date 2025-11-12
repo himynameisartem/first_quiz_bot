b73d93657e3a9b7dfaee28de5b0087e3c14b2d30
Artem Kudryavtsev <himynameisartem@icloud.com>
2025-11-12 13:31:55 +0300

    fix stats and db

--- database/db_handler.py
+++ database/db_handler.py
@@ -11,7 +11,6 @@ class DatabaseHandler:
                              (user_id INTEGER PRIMARY KEY, 
                              question_index INTEGER,
                              last_score INTEGER DEFAULT 0,
-                             current_score INTEGER DEFAULT 0,
                              total_games INTEGER DEFAULT 0,
                              best_score INTEGER DEFAULT 0)
                              ''')
@@ -26,28 +25,6 @@ class DatabaseHandler:
     async def get_quiz_index(self, user_id: int) -> int:
         async with aiosqlite.connect(self.db_path) as db:
             async with db.execute('SELECT question_index FROM quiz_state WHERE user_id = (?)', (user_id,)) as cursor:
-                results = await cursor.fetchone()
-                return results[0] if results is not None else 0
-
-    async def reset_current_score(self, user_id: int):
-        async with aiosqlite.connect(self.db_path) as db:
-            await db.execute('''INSERT INTO quiz_state (user_id, current_score) 
-                             VALUES (?, 0) 
-                             ON CONFLICT(user_id) DO UPDATE SET current_score = 0''',
-                             (user_id,))
-            await db.commit()
-
-    async def increment_current_score(self, user_id: int):
-        async with aiosqlite.connect(self.db_path) as db:
-            await db.execute('''INSERT INTO quiz_state (user_id, current_score) 
-                             VALUES (?, 1) 
-                             ON CONFLICT(user_id) DO UPDATE SET current_score = current_score + 1''',
-                             (user_id,))
-            await db.commit()
-
-    async def get_current_score(self, user_id: int) -> int:
-        async with aiosqlite.connect(self.db_path) as db:
-            async with db.execute('SELECT current_score FROM quiz_state WHERE user_id = ?', (user_id,)) as cursor:
                 results = await cursor.fetchone()
                 return results[0] if results is not None else 0
 
--- handlers/callbacks.py
+++ handlers/callbacks.py
@@ -15,6 +15,9 @@ class CallbackHandler:
         selected_answer = callback.data.split(":")[1]
         current_question_index = await self.db_handler.get_quiz_index(user_id)
 
+        if current_question_index == 0:
+            self.scores[user_id] = 0
+
         await callback.message.answer(f"✅ Ваш ответ: {selected_answer}")
         self.scores[user_id] = self.scores.get(user_id, 0) + 1
         current_question_index += 1
@@ -26,12 +29,17 @@ class CallbackHandler:
             score = self.scores.get(user_id, 0)
             await self.db_handler.save_result(user_id, score)
             await callback.message.answer(f"🎉 Квиз завершён!\nВаш результат: {score}/{len(quiz_data)}")
+            self.scores.pop(user_id, None)
 
     async def wrong_answer(self, callback: types.CallbackQuery):
         await self._clear_keyboard(callback)
         user_id = callback.from_user.id
         selected_answer = callback.data.split(":")[1]
         current_question_index = await self.db_handler.get_quiz_index(user_id)
+
+        if current_question_index == 0:
+            self.scores[user_id] = 0
+
         correct_option = quiz_data[current_question_index]['correct_option']
         correct_answer = quiz_data[current_question_index]['options'][correct_option]
 
@@ -46,6 +54,7 @@ class CallbackHandler:
             score = self.scores.get(user_id, 0)
             await self.db_handler.save_result(user_id, score)
             await callback.message.answer(f"🎉 Квиз завершён!\nВаш результат: {score}/{len(quiz_data)}")
+            self.scores.pop(user_id, None)
 
     async def _clear_keyboard(self, callback: types.CallbackQuery):
         await callback.bot.edit_message_reply_markup(
